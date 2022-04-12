10389e068e8cfadd6e939fb70d4d796cef0f90f0
JamsterMan <ben.brad.h@gmail.com>
2022-04-12 13:37:12 -0400

    added player melee and enemy
    -player attack
    -enemy character and health

---
+++ WaveSurvival/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public readonly int maxHealth = 10;
+    private int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        Debug.Log("enemy health: " + currentHealth);
+    }
+
+    public void DealDamage(int dmg)
+    {
+        currentHealth -= dmg;
+        Debug.Log("enemy health: " + currentHealth);
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        //play animation
+        Debug.Log("enemy Died");
+        Destroy(this.gameObject);
+    }
+}
---
+++ WaveSurvival/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Transform attackPoint;
+    public LayerMask enemyLayer;
+
+    public int attackDamage = 5;
+    public float attackRange = 0.5f;
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Debug.Log("player attacks");
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        //play animations
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach(Collider2D hitEnemy in hitEnemies)
+        {
+            Debug.Log("enemy hit: " + hitEnemy.name);
+            hitEnemy.GetComponent<Enemy>().DealDamage(attackDamage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
