7f3b36af0504c127b60736bd0b9b1fb39c83cdbc
JamsterMan <ben.brad.h@gmail.com>
2022-04-23 13:44:52 -0400

    changed waves
    -waves start when player walks over button (has temp art)
    -wave count increases

--- WaveSurvival/Assets/Scripts/WaveController.cs
+++ WaveSurvival/Assets/Scripts/WaveController.cs
@@ -1,28 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaveController : MonoBehaviour
 {
     public EnemySpawner[] spawners = new EnemySpawner[4];
+    public GameObject waveStart;
+    public Text waveCountText;
 
     public int maxEnemiesSpawn = 20;
     public float spawnRate = 0.5f;
-    public float waveBreakTime = 2f;
+    public float waveBreakTime = 0.2f;
 
     private int spawnedEnemyCount = 0;
     private float nextSpawnTime = 0f;
     private int count = 0;
     private float nextWaveTime = 2f;
 
+    private int waveCount = 0;
     private int enemiesDefeated = 0;
 
-    private bool inWave = true;
+    private bool inWave = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (inWave)
+        if (inWave && Time.time > nextWaveTime)
         {
             if (spawnedEnemyCount < maxEnemiesSpawn)
             {
@@ -39,16 +43,6 @@ public class WaveController : MonoBehaviour
                 }
             }
         }
-        else
-        {
-            if(Time.time > nextWaveTime)
-            {
-                spawnedEnemyCount = 0;
-
-                inWave = true;
-                Debug.Log("New wave start");
-            }
-        }
     }
 
     public void EnemyDied()
@@ -62,7 +56,23 @@ public class WaveController : MonoBehaviour
             Debug.Log("Wave ended");
 
             //enable wave start button
+            waveStart.SetActive(true);
         }
+    }
+
+    public void WaveStart()
+    {
+        spawnedEnemyCount = 0;
+
+        inWave = true;
+        Debug.Log("new wave started");
+        nextWaveTime = Time.time + waveBreakTime;
+
+        //increase wave count
+        waveCount++;
+        //update wave count UI
+        waveCountText.text = "" + waveCount;
+
     }
 
 }
---
+++ WaveSurvival/Assets/Scripts/WaveStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveStart : MonoBehaviour
+{
+    public WaveController waveController;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Wave Start");
+        waveController.WaveStart();
+        gameObject.SetActive(false);
+    }
+}
