d1966bdb25c900cf9380a474daf5f5f587c9bc50
JamsterMan <ben.brad.h@gmail.com>
2022-05-09 17:01:30 -0400

    added player Dodge

--- WaveSurvival/Assets/Scripts/PlayerMovement.cs
+++ WaveSurvival/Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+
     public Rigidbody2D rb;
     public Camera cam;
+    public float moveSpeed = 5f;
+    public float dodgeSpeed = 10f;
+    public float dodgeCooldown = 3f;
+    public float dodgeDuration = 1f;
+
+    private float nextDodgetime = 0f;
+    private float dodgeFinishTime = 0f;
+
+    [SerializeField]private bool isDodging = false;
+    [SerializeField] private bool canDodge = true;
 
     Vector2 movement;
     Vector2 mousePos;
@@ -14,21 +24,63 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        if (!isDodging)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
 
+        }
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetKeyDown(KeyCode.Space) && canDodge)//dodge
+        {
+            dodgeFinishTime = Time.time + dodgeDuration;
+            nextDodgetime = Time.time + dodgeCooldown;
+            isDodging = true;
+            canDodge = false;
+
+            if (movement.magnitude < 0.05)
+            {
+                Vector2 dodgeDirection = rb.position - mousePos;
+                movement = dodgeDirection.normalized;
+
+            }
+        }
+
+        //cooldown for dodge
+        if (!canDodge && Time.time > nextDodgetime)
+        {
+            canDodge = true;
+            Debug.Log("player can dodge");
+        }
     }
 
     // Update is called at a fixed framerate
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
         Vector2 lookDirection = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
 
         rb.rotation = angle;
+
+        if (isDodging)
+        {
+            PlayerDodge();
+        }
+        else
+        {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    private void PlayerDodge()
+    {
+        rb.MovePosition(rb.position + movement * dodgeSpeed * Time.fixedDeltaTime);
+
+        if (Time.time > dodgeFinishTime)
+        {
+            isDodging = false;
+        }
     }
 }
