d1945e30981e566d6732634e5d594a2b48f11c96
JamsterMan <ben.brad.h@gmail.com>
2022-05-13 14:26:18 -0400

    Wave start button updated
    -button will no longer activate if it spawns will the player is standing on its spawn location

--- WaveSurvival/Assets/Scripts/WaveStart.cs
+++ WaveSurvival/Assets/Scripts/WaveStart.cs
@@ -5,14 +5,41 @@ using UnityEngine;
 public class WaveStart : MonoBehaviour
 {
     public WaveController waveController;
+    public Transform playerPos;
+    public float safeDistance = 2f;
+    [SerializeField] private bool canBeUsed = true;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))//only start the wave if the player hits the button
+        if (other.gameObject.CompareTag("Player") && canBeUsed)//only start the wave if the player hits the button
         {
             Debug.Log("Wave Start");
             waveController.WaveStart();
             gameObject.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        //check if player is too close
+        if ((transform.position - playerPos.position).magnitude < safeDistance)
+        {
+            canBeUsed = false;
+            //change sprite to a different look
+        }
+        else
+        {
+            canBeUsed = true;
+            //change sprite to original look
+        }
+    }
+
+    private void Update()
+    {
+        if (!canBeUsed)
+        {
+            if((transform.position - playerPos.position).magnitude >= safeDistance)
+                    canBeUsed = true;
         }
     }
 }
