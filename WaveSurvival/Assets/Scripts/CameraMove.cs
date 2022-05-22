ff5ab7fe3408d5e80cf55ed2c635f81ee5652e47
JamsterMan <ben.brad.h@gmail.com>
2022-05-22 17:15:18 -0400

    added simple camera movement
    - used to move to the shop

---
+++ WaveSurvival/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Vector3 arenaPos;
+    public Vector3 shopPos;
+    public Transform cameraPostion;
+    private bool isInShop = false;
+
+    private void MoveCameraPosition()
+    {
+        if (isInShop)
+        {
+            cameraPostion.position = arenaPos;
+        }
+        else
+            cameraPostion.position = shopPos;
+
+        isInShop = !isInShop;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            MoveCameraPosition();
+        }
+    }
+}
