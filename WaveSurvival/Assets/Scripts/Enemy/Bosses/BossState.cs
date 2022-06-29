d8b6e68ee06523a9c677d2fdd49c610826927977
JamsterMan <ben.brad.h@gmail.com>
2022-06-29 15:30:51 -0400

    boss state
    started making boss states

---
+++ WaveSurvival/Assets/Scripts/Enemy/Bosses/BossState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossState : MonoBehaviour
+{
+    private enum State {chase, attack, charge, p2Chase, p2Attack, p2Charge}
+    private State bossState = State.chase;
+
+    private void Update()
+    {
+        if(bossState == State.chase)//chase player
+        {
+
+        }
+        else if (bossState == State.attack)//attack the player
+        {
+
+        }
+        else if (bossState == State.charge)//charge at the player
+        {
+
+        }
+        else if (bossState == State.p2Chase)//faster chase
+        {
+
+        }
+        else if (bossState == State.p2Attack)//stronger attack
+        {
+
+        }
+        else if (bossState == State.p2Charge)//faster charge
+        {
+
+        }
+    }
+
+}
